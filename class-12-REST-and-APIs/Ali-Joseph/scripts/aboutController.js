355496f7071cb57c23c89f84ef4b6c6287a8b3c7
AlegriaForman <alegriaforman@gmail.com>
2016-02-16 13:01:01 -0800

    add github API request and update About page for Class-12

---
+++ class-12-REST-and-APIs/Ali-Joseph/scripts/aboutController.js
@@ -0,0 +1,11 @@
+(function(module) {
+  var aboutController = {};
+
+  aboutController.index = function() {
+    // TODO: Call a function to load all the data.
+    // Pass a view function as a callback, so the view will render after the data is loaded.
+    repos.requestRepos(repoView.index);
+  };
+
+  module.aboutController = aboutController;
+})(window);
